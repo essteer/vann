fd9c11695c5125906acafebdd598daf29e7f1023
essteer <essteer@pm.me>
2024-08-20 17:02:59 +0800

    chore: First commit

---
+++ src/main/java/com/vann/VannApplication.java
@@ -0,0 +1,13 @@
+package com.vann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VannApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VannApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vann/VannApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VannApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
