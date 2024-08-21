821e28299a03a2a5f527c2968a4ead79b12fded1
essteer <essteer@pm.me>
2024-08-21 09:07:12 +0800

    fix: Add @Repository annotations to InvoiceRepo and ProductRepo interfaces

--- src/main/java/com/vann/repositories/InvoiceRepo.java
+++ src/main/java/com/vann/repositories/InvoiceRepo.java
@@ -3,9 +3,11 @@ package com.vann.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.vann.model.Invoice;
 
+@Repository
 public interface InvoiceRepo extends JpaRepository<Invoice, UUID> {
 
 }
--- src/main/java/com/vann/repositories/ProductRepo.java
+++ src/main/java/com/vann/repositories/ProductRepo.java
@@ -3,9 +3,11 @@ package com.vann.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.vann.model.Product;
 
+@Repository
 public interface ProductRepo extends JpaRepository<Product, UUID> {
 
 }
