6852d196499adcdcabd21a12694661f1f3d2a9dc
essteer <essteer@pm.me>
2024-08-27 09:21:39 +0800

    test: Update unit test to include CategoryType check

--- src/test/java/com/vann/model/CategoryTest.java
+++ src/test/java/com/vann/model/CategoryTest.java
@@ -24,12 +24,14 @@ public class CategoryTest {
         defaultCategory.generateIdIfAbsent();
         assertNotNull(defaultCategory.getCategoryId(), "UUID should be generated");
         assertNull(defaultCategory.getCategoryName(), "Category name should be null");
+        assertNull(defaultCategory.getCategoryType(), "Category type should be null");
     }
 
     @Test
     public void testCategoryParameterizedConstructor() {
         assertNotNull(category.getCategoryId(), "UUID should be generated");
         assertEquals("jewellery", category.getCategoryName(), "Category name should match");
+        assertEquals(CategoryType.JEWELLERY, category.getCategoryType(), "Category type should match");
     }
 
     @Test
@@ -37,9 +39,11 @@ public class CategoryTest {
         UUID newUuid = UUID.randomUUID();
         category.setCategoryId(newUuid);
         category.setCategoryName("rings");
-
-        assertEquals(newUuid, category.getCategoryId(), "UUID should match");
+        category.setCategoryType(CategoryType.RING);
+        
         assertEquals("rings", category.getCategoryName(), "Category name should match");
+        assertEquals(newUuid, category.getCategoryId(), "UUID should match");
+        assertEquals(CategoryType.RING, category.getCategoryType(), "Category type should match");
     }
 
     @Test
