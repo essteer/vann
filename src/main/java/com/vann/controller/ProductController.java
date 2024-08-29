c67ab1c16262c210a7dfd3ff99436bced6139e1a
essteer <essteer@pm.me>
2024-08-29 10:55:07 +0800

    feat: Add CORS for localhost:3000 on ProductController GET requests

--- src/main/java/com/vann/controller/ProductController.java
+++ src/main/java/com/vann/controller/ProductController.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,7 @@ public class ProductController {
     @Autowired
     private CategoryService categoryService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
         List<Product> products = productService.findAllProducts();
@@ -47,24 +49,28 @@ public class ProductController {
         return ResponseEntity.ok(products);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/category/name/{categoryName}")
     public ResponseEntity<List<Product>> getProductsByCategoryName(@PathVariable String categoryName) {
         List<Product> products = productService.findProductsByCategoryName(categoryName);
         return ResponseEntity.ok(products);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/category/type/{categoryType}")
     public ResponseEntity<List<Product>> getProductsByCategoryType(@PathVariable CategoryType categoryType) {
         List<Product> products = productService.findProductsByCategoryType(categoryType);
         return ResponseEntity.ok(products);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/name/{productName}")
     public ResponseEntity<List<Product>> getProductsByName(@PathVariable String productName) {
         List<Product> products = productService.findProductsByName(productName);
         return ResponseEntity.ok(products);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/{id}")
     public ResponseEntity<?> getProduct(@PathVariable UUID id) {
         try {    
