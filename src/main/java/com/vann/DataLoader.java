9a02effbc73f16c14567def468b38bc25645c453
essteer <essteer@pm.me>
2024-08-30 16:34:07 +0800

    log: Add initial LogHandler class and log success and fail for DataLoader class

--- src/main/java/com/vann/DataLoader.java
+++ src/main/java/com/vann/DataLoader.java
@@ -28,17 +28,30 @@ public class DataLoader implements ApplicationRunner {
     }
 
     @Override
-    public void run(ApplicationArguments args) {
+    public void run(ApplicationArguments args) throws RuntimeException {
 
-        @SuppressWarnings("unused")
-        CategoryService categoryService = new CategoryService(this.categoryRepo);
-        ProductService productService = new ProductService(this.productRepo);
-        CustomerService customerService = new CustomerService(this.customerRepo);
-        InvoiceItemService invoiceItemService = new InvoiceItemService(this.invoiceItemRepo, productService);
-        InvoiceService invoiceService = new InvoiceService(customerService, invoiceItemService, this.invoiceRepo);
-        @SuppressWarnings("unused")
-        CartService cartService = new CartService(this.cartRepo, customerService, invoiceService, productService);
+        try {
+            CategoryService categoryService = new CategoryService(this.categoryRepo);
+            LogHandler.serviceClassInitOK(categoryService.getClass().getName());
+            
+            ProductService productService = new ProductService(this.productRepo);
+            LogHandler.serviceClassInitOK(productService.getClass().getName());
+            
+            CustomerService customerService = new CustomerService(this.customerRepo);
+            LogHandler.serviceClassInitOK(customerService.getClass().getName());
+            
+            InvoiceItemService invoiceItemService = new InvoiceItemService(this.invoiceItemRepo, productService);
+            LogHandler.serviceClassInitOK(invoiceItemService.getClass().getName());
+            
+            InvoiceService invoiceService = new InvoiceService(customerService, invoiceItemService, this.invoiceRepo);
+            LogHandler.serviceClassInitOK(invoiceService.getClass().getName());
+            
+            CartService cartService = new CartService(this.cartRepo, customerService, invoiceService, productService);
+            LogHandler.serviceClassInitOK(cartService.getClass().getName());
         
+        } catch (Exception e) {
+            LogHandler.serviceClassInitError(e.getMessage());
+            throw new RuntimeException();
+        }
     }
-
 }
